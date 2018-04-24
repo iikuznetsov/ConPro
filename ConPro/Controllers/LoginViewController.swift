f8ba252694975600c194b834d46083130a4c4c37
Maria Korovina <eyentei@gmail.com>
2018-04-24 20:55:14 +0300

    Initial Commit

---
+++ ConPro/Controllers/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  ConPro
+//
+//  Created by Мария Коровина on 24.04.2018.
+//  Copyright © 2018 ConPro. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ ConPro/Controllers/RegisterViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RegisterViewController.swift
+//  ConPro
+//
+//  Created by Мария Коровина on 24.04.2018.
+//  Copyright © 2018 ConPro. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
