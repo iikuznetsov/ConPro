b531826d74976fe702053a1397245374177b96f8
Maria Korovina <eyentei@gmail.com>
2018-04-28 13:14:32 +0300

    Login and register
    Add alomofire
    Creater controllers for registration/login

--- ConPro/Controllers/LoginViewController.swift
+++ ConPro/Controllers/LoginViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import Alamofire
 
 class LoginViewController: UIViewController {
-
+    let url = "https://api.beheltcarrot.ru/account/GetToken"
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        statusLabel.text = ""
+        emailTextField.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
     }
+    
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBAction func logIn(_ sender: UIButton) {
+        
+        guard let email = emailTextField.text, let password = passwordTextField.text,
+            !email.isEmpty, !password.isEmpty else {
+                statusLabel.text = "Please fill in both fields"
+                return
+        }
 
+        Alamofire.request(url, method: .post, parameters:["Email":email,"Password":password])
+            .responseData {
+            response in
+            self.statusLabel.text = response.error?.localizedDescription
+            print(response)
+            }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @objc func editingChanged(_ textField: UITextField) {
+        
+        statusLabel.text = ""
+
+        textField.text = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if (textField.text?.isEmpty)!
+        {
+            textField.layer.cornerRadius = 8.0;
+            textField.layer.masksToBounds = true;
+            textField.layer.borderColor = UIColor.red.cgColor;
+            textField.layer.borderWidth = 2.0;
+        }
+        else
+        {
+            textField.layer.borderColor = UIColor.clear.cgColor;
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
--- ConPro/Controllers/RegisterViewController.swift
+++ ConPro/Controllers/RegisterViewController.swift
@@ -7,16 +7,64 @@
 //
 
 import UIKit
+import Alamofire
 
 class RegisterViewController: UIViewController {
 
+    let url = "https://api.beheltcarrot.ru/account/Register"
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatedPasswordTextField: UITextField!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBAction func register(_ sender: UIButton) {
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        
+        Alamofire.request(url, method: .post, parameters:["Email":email,"Password":password])
+            .responseData {
+                response in
+                self.statusLabel.text = response.error?.localizedDescription
+                print(response)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        statusLabel.text = ""
+        emailTextField.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
+        firstNameTextField.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
+        lastNameTextField.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
+        repeatedPasswordTextField.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
     }
 
+    @objc func editingChanged(_ textField: UITextField) {
+        
+        statusLabel.text = ""
+
+        if passwordTextField.text != repeatedPasswordTextField.text && !(repeatedPasswordTextField.text?.isEmpty)! {
+            statusLabel.text = "Passwords are not the same"
+        }
+        
+        textField.text = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if (textField.text?.isEmpty)!
+        {
+            textField.layer.cornerRadius = 8.0;
+            textField.layer.masksToBounds = true;
+            textField.layer.borderColor = UIColor.red.cgColor;
+            textField.layer.borderWidth = 2.0;
+        }
+        else
+        {
+            textField.layer.borderColor = UIColor.clear.cgColor;
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-
 }
